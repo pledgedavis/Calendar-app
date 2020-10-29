755d0e9cb93361ccb035009a33924296ba475dcf
pledgedavis <pledgedavis14@gmail.com>
2020-10-28 23:07:11 -0400

    Added fetch and started html file page

---
+++ frontend/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+fetchMonths()
+
+});
+
+function fetchMonths(){
+    fetch("http://localhost:3000/months")
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(json){
+         console.log(json)
+      });
+      }
