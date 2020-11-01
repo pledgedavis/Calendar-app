2f922c9294d11c364368440d5a6d7c1ea504cb6e
pledgedavis <pledgedavis14@gmail.com>
2020-11-01 16:26:44 -0500

    added days constructor

--- frontend/day.js
+++ frontend/day.js
@@ -1,3 +1,16 @@
 class Day{
+    // let daysDiv = document.getElementById("day-form")
 
+    constructor(id, name, task, month_id, priority, length){
+        this.id = id
+        this.name = name;
+        this.task = task;
+        this.month = month_id;
+        this.priority = priority;
+        this.length = length;
+    }
+
+
+
+    
 }
