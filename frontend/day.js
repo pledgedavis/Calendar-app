47fae21f8d144b693770089d059776a7d22fe3a3
pledgedavis <pledgedavis14@gmail.com>
2020-11-01 19:36:36 -0500

    adding days form

--- frontend/day.js
+++ frontend/day.js
@@ -5,12 +5,26 @@ class Day{
         this.id = id
         this.name = name;
         this.task = task;
-        this.month = month_id;
+        this.month_id = month_id;
         this.priority = priority;
         this.length = length;
     }
 
+   
 
+
+//    function createForm(){
+//       let daysDiv = document.getElementById("day-form")
+     
+//       daysDiv.innerHTML +=
+//       <form onsubmit="setTimeout(function)){window.location.reload();},10);">
+//            Name: <input type="text" id="name"><br><br>
+//            Task: <input type="text" id="task"><br><br>
+//            month: <input type="month_id" id="month_id"><br>
+//            priority: <input type="number" id="priority" style="width: 40px" min="1" max="12"> 
+//         length: <input type="number" id="length" style="width: 40px" min="1" max="31"><br><br>
+//         <input type="submit" value="Find Your Zodiac Sign"></input>
+//       </form>
 
     
 }
--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,12 @@
 const BASE_URL = "http://localhost:3000/months"
 
 document.addEventListener("DOMContentLoaded", () => {
-fetchFormMonths()
-
+fetchMonths()
+createDaysForm()
 });
 
 
-function fetchFormMonths(){
+function fetchMonths(){
     fetch(`${BASE_URL}`)
     .then(response => response.json())
        .then(months =>{
@@ -16,26 +16,44 @@ function fetchFormMonths(){
           }
         })
 
-    
-    // function grabUsers() {
-    //     fetch(`${BASE_URL}/users`)
-    //     .then(res => res.json())
-    //     .then(users =>{
-    //         for (let user of users){
-    //             let u = new User(user.id, user.username, user.total)
-    //             u.renderUser();
-    //         }
-    //     })
-    // } 
 
+     
     
-
     // const rendermonth(){
     //     Month.all.forEach(m => m.name)
-    // }
+    // }  
+}
 
+
+function createDaysForm(){
+    let daysDiv = document.getElementById("day-form")
+   
+  
+}
+
+
+
+function daysFormSubmit(){
+
+    event.preventDefault();
+
+    let name = document.getElementById("name").value
+ 
+    let task = document.getElementById("task").value
+
+    let month_id = document.getElementById("month_id").value
+    
+    let priority = document.getElementById("priority").value
+
+    let user = {
+        name: name,
+        task: task,
+        month_id: month_id,
+        priority: priority,
     
 }
+}
+
 
 const renderMonth = () => {
     Month.all.forEach(m => m.name)
