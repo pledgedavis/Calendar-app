cfa12a6cf920c3b82a370da2d7ed691d24f75a79
Pledge Davis <pledgedavis14@gmail.com>
2021-06-24 01:32:14 -0400

    added maximum length of day  validation

--- backend/app/models/day.rb
+++ backend/app/models/day.rb
@@ -4,7 +4,7 @@ class Day < ApplicationRecord
     validates :name, :day_number, :task, :priority, :length, presence: true
     validates :name, length: { maximum: 9 }
     validates :task, length: { maximum: 30 }
-    
+    validates :length, length: { maximum: 20 }
     
 
 
